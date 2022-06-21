836d0aab78ee9fb4155c232a1a8f1c58864bff82
ImmortalCount <devnaut@gmail.com>
2022-06-21 16:04:20 -0700

    items now displayed at home

---
+++ utils/data.js
@@ -0,0 +1,67 @@
+
+const data = {
+    products: [
+        {
+            name: 'The official Noodler Book v.1',
+            slug: 'Master the fretboard',
+            category: 'Books',
+            image: 'images/book1.jpg',
+            price: 1995,
+            brand: 'Noodler',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'The official Noodler book. Master the fretboard and shave years off of your practice.'
+        },
+        {
+            name: 'The official Noodler Book v.2',
+            slug: 'Master the fretboard',
+            category: 'Books',
+            image: 'images/book2.jpg',
+            price: 1995,
+            brand: 'Noodler',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'The official Noodler book. Master the fretboard and shave years off of your practice.'
+        },
+        {
+            name: 'The official Noodler Book v.3',
+            slug: 'Master the fretboard',
+            category: 'Books',
+            image: 'images/book3.jpg',
+            price: 1995,
+            brand: 'Noodler',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'The official Noodler book. Master the fretboard and shave years off of your practice.'
+        },
+        {
+            name: 'The official Noodler Book v.4',
+            slug: 'Master the fretboard',
+            category: 'Books',
+            image: 'images/book4.jpg',
+            price: 1995,
+            brand: 'Noodler',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'The official Noodler book. Master the fretboard and shave years off of your practice.'
+        },
+        {
+            name: 'The official Noodler Book v.5',
+            slug: 'Master the fretboard',
+            category: 'Books',
+            image: 'images/book5.jpg',
+            price: 1995,
+            brand: 'Noodler',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'The official Noodler book. Master the fretboard and shave years off of your practice.'
+        },
+    ]
+}
+
+export default data
